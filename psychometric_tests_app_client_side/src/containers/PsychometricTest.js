43665366974cd15fd32439dfe9252d33332a11ed
sara <smhumphries@hotmail.co.uk>
2021-08-09 14:59:17 +0100

    clicks through questions

--- psychometric_tests_app_client_side/src/containers/PsychometricTest.js
+++ psychometric_tests_app_client_side/src/containers/PsychometricTest.js
@@ -5,9 +5,17 @@ const PsychometricTest = function({psychometricTest}) {
 
     const[currentQuestion, setCurrentQuestion] = useState(psychometricTest.questions[0]);
 
-    useEffect(() => {
-        setCurrentQuestion(psychometricTest.questions[0]);
-    });
+    const moveToNextQuestion = function() {
+        const currentQuestionIndex = psychometricTest.questions.indexOf(currentQuestion);
+        const nextQuestion = psychometricTest.questions[currentQuestionIndex+1];
+        setCurrentQuestion(nextQuestion);
+    };
+
+    // useEffect(() => {
+    //     setCurrentQuestion(psychometricTest.questions[0]);
+    // });
+
+    
 
     const QuestionList = psychometricTest.questions.map((question) => {
         return <Question question = {question}></Question>
@@ -17,6 +25,7 @@ const PsychometricTest = function({psychometricTest}) {
         <div>
             <div>{psychometricTest.title}</div>
             {currentQuestion.questionText}
+            <button onClick = {moveToNextQuestion}>Next Question</button>
             {/* {QuestionList} */}
         </div>
     );
