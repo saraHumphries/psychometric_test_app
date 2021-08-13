d4758e3313335d20e82b47f58c3c2da8a73aba59
sara <smhumphries@hotmail.co.uk>
2021-08-13 15:19:02 +0100

    no change

--- psychometric_tests_app_client_side/src/components/Result.js
+++ psychometric_tests_app_client_side/src/components/Result.js
@@ -2,6 +2,7 @@
 const Result = function({answer}) {
     return (
         <div>
+            <p>Question id: {answer.question.id}</p>
             <p>{answer.question.questionText}</p>
             <p> Your response: {answer.response}</p>
         </div>
