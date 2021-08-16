880e63010ffef8ab2623c150e7a571602940cda7
sara <smhumphries@hotmail.co.uk>
2021-08-16 11:05:53 +0100

    creates graphs for population data per question

--- psychometric_tests_app_client_side/src/components/ChartDisplay.js
+++ psychometric_tests_app_client_side/src/components/ChartDisplay.js
@@ -3,19 +3,21 @@ import Chart from "react-google-charts";
 
 const ChartDisplay = function({questionSummary}) {
 
+    console.log('questionSummary', questionSummary[1]);
+
     return (
         <div>
             <Chart
-                width={400}
+                width={800}
                 height={300}
                 chartType = 'ColumnChart'
                 data = {[
                     ['response', 'count'],
-                    ['agree', 115],
-                    ['slightly agree', 300],
-                    ['neither', 455],
-                    ['slightly disagree', 300],
-                    ['disgaree', 100]
+                    ['agree',questionSummary[1]],
+                    ['slightly agree', questionSummary[2]],
+                    ['neither', questionSummary[3]],
+                    ['slightly disagree', questionSummary[4]],
+                    ['disgaree', questionSummary[5]]
                 ]}
             ></Chart>
         </div>
