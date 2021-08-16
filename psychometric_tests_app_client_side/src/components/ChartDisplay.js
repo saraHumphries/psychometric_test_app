16dc3846a526c4363bf79a93d484e5170662a942
sara <smhumphries@hotmail.co.uk>
2021-08-16 17:06:50 +0100

    sets colours for bar chart bars

--- psychometric_tests_app_client_side/src/components/ChartDisplay.js
+++ psychometric_tests_app_client_side/src/components/ChartDisplay.js
@@ -18,16 +18,38 @@ const ChartDisplay = function({likertOptions, questionSummary, response}) {
         };
     };
 
+    const getColor = function(likertValue) {
+        let color = 'grey';
+        if(likertValue === 1) {
+            color = '#BF3A2B';
+        }
+        else if(likertValue === 2) {
+            color = '#E77E23';
+        }
+        else if(likertValue == 3) {
+            color = '#3499DC';
+        }
+        else if(likertValue == 4) {
+            color = '#16A186';
+        }
+        else if(likertValue === 5) {
+            color = '#2ECD71';
+        }
+        return color;
+    };
+
+
+
     const getDataForChart = function() {
         if(likertOptions) {
             const dataForChart = likertOptions.map((likertOption) => {
-                return [likertOption.likertText, questionSummary[likertOption.likertValue], 'color: grey; ' + `opacity: ${getOpacities()[`${likertOption.likertValue}`]}`]
+                return [likertOption.likertText, questionSummary[likertOption.likertValue], `color: ${getColor(likertOption.likertValue)}; ` + `opacity: ${getOpacities()[`${likertOption.likertValue}`]}`]
             });
             dataForChart.unshift(['response', 'count', {role: 'style'}]);
             return dataForChart;
         };
     };
-    
+
     return (
         <div>
             {questionSummary? 
