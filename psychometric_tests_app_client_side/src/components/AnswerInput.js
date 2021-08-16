c48cff18e8966cc307e8b5ee7c77458b602a66d2
sara <smhumphries@hotmail.co.uk>
2021-08-16 15:50:48 +0100

    sets colors for LikertButtons"

--- psychometric_tests_app_client_side/src/components/AnswerInput.js
+++ psychometric_tests_app_client_side/src/components/AnswerInput.js
@@ -17,7 +17,7 @@ const AnswerInput = function({likertOptions, psychometricTest, saveAnswer, endOf
     const getLikertButtons = function() {
         if(likertOptions) {
             const likertButtons = likertOptions.map((likertOption) => {
-                return <button className='button' value={likertOption.likertValue} onClick={onLikertButtonClick}>{likertOption.likertText}</button>
+                return <button className='button' id={"likert-button" + likertOption.likertValue} value={likertOption.likertValue} onClick={onLikertButtonClick}>{likertOption.likertText}</button>
             });
             return likertButtons;
         };
