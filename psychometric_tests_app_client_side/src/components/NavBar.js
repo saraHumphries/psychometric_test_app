e4e1d9e863d0e2f478bfb17af1ad632b54a3768b
sara <smhumphries@hotmail.co.uk>
2021-08-16 13:50:05 +0100

    removes NRS summary from NavBar

--- psychometric_tests_app_client_side/src/components/NavBar.js
+++ psychometric_tests_app_client_side/src/components/NavBar.js
@@ -6,7 +6,6 @@ const NavBar = function() {
         <nav>
             <ul id='nav-bar'>
                 <Link to='/'>Home</Link>
-                <Link to='/NRScale_summary'>NRScale Summary</Link>
             </ul>
             <h1>Psychometric tests app</h1>
             
