82762785470435976589c1581c560a19b16a8309
sara <smhumphries@hotmail.co.uk>
2021-08-17 13:37:08 +0100

    likert options hidden until title is set

--- psychometric_tests_app_client_side/src/containers/CreateTestPage.js
+++ psychometric_tests_app_client_side/src/containers/CreateTestPage.js
@@ -13,6 +13,18 @@ const CreateTestPage = function() {
         };
         PsychometricTestsService.postPsychometricTest(psychometricTestToPost)
             .then(res => setNewPsychometricTest(res));
+        hideTitleForm();
+        makeLikertInputsVisible();
+    };
+
+    const hideTitleForm = function() {
+        const titleForm = document.getElementById('title-form');
+        titleForm.style.display = 'none';
+    };
+
+    const makeLikertInputsVisible = function() {
+        const likertInputs = document.getElementById('likert-options-section');
+        likertInputs.style.display = 'block';
     };
 
     return (
@@ -25,13 +37,20 @@ const CreateTestPage = function() {
                     <button id='submit-title-button' onClick={onTitleSubmit}>next</button>
                 </section>
 
-                <section id='likert-options-form'>
-                    <input className='likert_input' id='likert_option_1' type='text' placeholder='e.g strongly disagree'></input>
-                    <input className='likert_input' id='likert_option_2' type='text' placeholder='e.g disgaree'></input>
-                    <input className='likert_input' id='likert_option_3' type='text' placeholder='e.g neither'></input>
-                    <input className='likert_input' id='likert_option_4' type='text' placeholder='e.g disagree'></input>
-                    <input className='likert_input' id='likert_option_5' type='text' placeholder='e.g strongly disagree'></input>
-                </section>
+                {newPsychometricTest? <h2 id='title-set-title'>{newPsychometricTest.title}</h2>: null}
+
+                <div id='likert-options-section'>
+                    <div>
+                        <p>Fill in what you want</p>
+                    </div>
+                    <section id='likert-options-form'>
+                        <input className='likert_input' id='likert_option_1' type='text' placeholder='e.g strongly disagree'></input>
+                        <input className='likert_input' id='likert_option_2' type='text' placeholder='e.g disgaree'></input>
+                        <input className='likert_input' id='likert_option_3' type='text' placeholder='e.g neither'></input>
+                        <input className='likert_input' id='likert_option_4' type='text' placeholder='e.g disagree'></input>
+                        <input className='likert_input' id='likert_option_5' type='text' placeholder='e.g strongly disagree'></input>
+                    </section>
+                </div>
             </div>
         
     );
