57cf190ddc7c74af4f26abfbcf8eef4f83f89f7e
sara <smhumphries@hotmail.co.uk>
2021-08-17 13:26:47 +0100

    adds input boxes for likerts

--- psychometric_tests_app_client_side/src/containers/CreateTestPage.js
+++ psychometric_tests_app_client_side/src/containers/CreateTestPage.js
@@ -19,9 +19,19 @@ const CreateTestPage = function() {
 
         
             <div>
-                <label htmlFor='test-title'>Test title</label>
-                <input type='text' id='test-title'></input>
-                <button id='submit-title-button' onClick={onTitleSubmit}>next</button>
+                <section id='title-form'>
+                    <label htmlFor='test-title'>Test title</label>
+                    <input type='text' id='test-title'></input>
+                    <button id='submit-title-button' onClick={onTitleSubmit}>next</button>
+                </section>
+
+                <section id='likert-options-form'>
+                    <input className='likert_input' id='likert_option_1' type='text' placeholder='e.g strongly disagree'></input>
+                    <input className='likert_input' id='likert_option_2' type='text' placeholder='e.g disgaree'></input>
+                    <input className='likert_input' id='likert_option_3' type='text' placeholder='e.g neither'></input>
+                    <input className='likert_input' id='likert_option_4' type='text' placeholder='e.g disagree'></input>
+                    <input className='likert_input' id='likert_option_5' type='text' placeholder='e.g strongly disagree'></input>
+                </section>
             </div>
         
     );
